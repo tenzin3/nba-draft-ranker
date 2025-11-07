98fda2a88973e9c2a445099a7e4452ddc461ebe7
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-11-07 12:33:18 -0500

    scrap draft from 2000-2025

--- scrap.py
+++ scrap.py
@@ -1,15 +1,25 @@
+from pathlib import Path 
+from tqdm import tqdm
+
 from nba_api.stats.endpoints import drafthistory
 
+DRAFT_DIR = Path("drafts")
+DRAFT_DIR.mkdir(exist_ok=True)
+
 class Srapper:
-    def __init__(self):
-        pass 
+    def __init__(self, draft_dir:Path = DRAFT_DIR):
+        self.draft_dir = draft_dir
 
     def scrap(self, year:int):
         # nba_api expects a string year via season_year_nullable
         draft = drafthistory.DraftHistory(season_year_nullable=str(year)) 
         return draft.get_data_frames()[0]
 
+    def scrap_all(self):
+        for year in tqdm(range(2000, 2026), desc="Scraping drafts History"):
+            df = self.scrap(year)
+            df.to_csv(self.draft_dir / f"draft_{year}.csv", index=False)
+
 if __name__ == "__main__":
     scraper = Srapper()
-    df_2025 = scraper.scrap(year=2025)
-    df_2025.to_csv("draft_2025.csv", index=False)
+    scraper.scrap_all()
